715810b29fa9102ab5e5cf3d72e4c535963e2bd6
pulkitsujaan <pulkitsujaan2708@gmail.com>
2023-11-11 21:36:16 +0530

    yes

---
+++ TestCode/areaofcirlce.c
@@ -0,0 +1,12 @@
+//Area of circle
+#include <stdio.h>
+int main ()
+{
+    float a,r;
+    printf("the value of r: ");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("the area of circle is %f",a);
+    return 0;
+
+}
