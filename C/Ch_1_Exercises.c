d667b937b7149b6eb82cfb349ed9a1a08122d30b
pulkitsujaan <pulkitsujaan2708@gmail.com>
2024-06-15 16:59:59 +0530

    hello

--- C/Ch_1_Exercises.c
+++ C/Ch_1_Exercises.c
@@ -5,21 +5,22 @@ Exercise [F]
 */
 
 ////(a)
-//#include<stdio.h>
-//int main(){
-//	int i=2,j=3,k,l;
-//	float a,b;
-//	k=i/j*j;//	2/3*3 = 0*3 = 0
-//	l=j/i*i;//	3/2*2 = 1*2 = 2
-//	a=i/j*j;//	2/3*3 = 0*3 = 0.000000
-//	b=j/i*i;//	3/2*2 = 1*2 = 2.000000
-//	printf("%d\n%d\n%f\n%f",k,l,a,b);
-//}
+#include<stdio.h>
+int main(){
+	int i=2,j=3,k,l;
+	float a,b;
+	k=i/j*j;//	2/3*3 = 0*3 = 0
+	l=j/i*i;//	3/2*2 = 1*2 = 2
+	a=i/j*j;//	2/3*3 = 0*3 = 0.000000
+	b=j/i*i;//	3/2*2 = 1*2 = 2.000000
+	printf("%d\n%d\n%f\n%f",k,l,a,b);
+}
 
 
 ////(b)
 //#include<stdio.h>
-//int main(){
+//int main()
+//{
 //	int a,b;
 //	a = -3 - -3;
 //	b= -3 - -(-3);
